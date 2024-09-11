403891fc12df175cc6f817688ba46d8c1e654f78
Sören Kuklau <kuklau@trenz.de>
2024-09-11 09:01:55 +0200

    add project

---
+++ Chucker.Blazor.BuildTasks.GitHashedIndexHtml/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.Build.Utilities;
+
+namespace Chucker.Blazor.BuildTasks.GitHashedIndexHtml;
+
+public class Class1:Task
+{
+    public override bool Execute()
+    {
+        throw new NotImplementedException();
+    }
+}
